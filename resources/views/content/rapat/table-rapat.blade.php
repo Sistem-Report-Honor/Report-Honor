868de27e275d57de1aba64527e622e713f107ef7
248ForReal <masrauf123@gmail.com>
2024-04-04 11:14:44 +0700

    feat : print qr & print Report

--- resources/views/content/rapat/table-rapat.blade.php
+++ resources/views/content/rapat/table-rapat.blade.php
@@ -95,6 +95,7 @@
                                     </form>
                                 @endif
                             </div>
+                            <a href="{{ route('print.qr', $rapat->id) }}" target="_blank" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">Print QR Code</a>
                         </div>
                     </div>
                 @endforeach
